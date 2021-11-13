f21b59b72d6a14fc41081c9e39fa6697b20562eb
Stevie Ambroise <s.ambroise@protonmail.com>
2021-11-13 14:21:48 -0700

    Add beginning game logic and choose fighter functions

--- src/game.js
+++ src/game.js
@@ -1,14 +1,41 @@
 class Game {
   constructor(type) {
     this.type = type;
-    this.player = new Player([with the arguments]);
-    this.computer = new Player([with the arguments, choices are randomized]);
-
+    this.player = new Player('human', '🙎🏾‍♀️');
+    this.computer = new Player('computer', '🖥');
+    this.winner = "";
   }
+
+// A way to keep track of the data for the game board
+// A way to keep track of the selected game type
+// A way to check the Game’s board data for win conditions
+// A way to detect when a game is a draw (no one has won)
+// A way to reset the Game’s board to begin a new game
 
   playGame() {
 
   }
+
+  setGameType(type) {
+    this.type = type;
+    this.computer.chooseRandomFighter(type)
+  }
+
+  checkWinner() {
+    var playerChoice = this.player.choice;
+    var computerChoice = this.computer.choice;
+    if (playerChoice === 'book' && computerChoice === 'broom')
+      this.player.wins++;
+      this.winner = this.player.name;
+  }
+
+  updateWinText() {
+    chooseYourGameTitleLine.innerText = `${this.winner}`
+    //you also need to display chosen icons (inner HTML);
+    //this.player.choice
+  }
+
+
 }
 
 
@@ -25,11 +52,3 @@ class Game {
 //update innerText to equal "Human wins!";
 //} else if(this.player.choice === scissors && this.computer.choice === paper) {
 //
-
-}
-
-}
-
-
-
-}
