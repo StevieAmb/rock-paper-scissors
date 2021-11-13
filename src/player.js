9359c4a3121620ffd1c5242d6bd3ba61d5cab185
Stevie Ambroise <s.ambroise@protonmail.com>
2021-11-12 19:21:52 -0700

    Add player class

---
+++ src/player.js
@@ -0,0 +1,32 @@
+class Player {
+  constructor(name, image, choice) {
+    this.name = name;
+    this.emoji = image;
+    this.choice = choice;
+    this.wins = 0;
+  }
+
+  saveWinsToStorage() {
+
+  }
+
+  retrieveWinsFromStorage() {
+
+  }
+
+  takeTurn() {
+    this.choice =
+    this.choice =
+  }
+}
+
+//there's game logic, and then there's playing the game
+//to activate the game logic, each player has to take a take
+//to take a turn, the human clicks the button
+//the computer randomizes choices. So in an object instance
+//of the computer, choices is choices[getRandomIndex(choices)]
+//figure out how to get to my choices
+
+
+//your player class has to be used to remake the the player sections
+//of the comp. (so use innerHTML to put the character up)
