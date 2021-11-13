0f68031cb7f4a4673cc0aef854c1e8063ecaa818
Stevie Ambroise <s.ambroise@protonmail.com>
2021-11-12 19:19:24 -0700

    Add the game class

---
+++ src/game.js
@@ -0,0 +1,35 @@
+class Game {
+  constructor(type) {
+    this.type = type;
+    this.player = new Player([with the arguments]);
+    this.computer = new Player([with the arguments, choices are randomized]);
+
+  }
+
+  playGame() {
+
+  }
+}
+
+
+//Game Logic
+//This could be an ELSE??? //if (this.player.choice === this.computer.choice) {
+// update the innerText of Choose Your Game to equal "It's a Draw"
+//this.player.wins
+//this.computer.wins
+//else if(this.player.choice === paper && this.computer.choice === rock) {
+//this.player.wins++;
+//update innerText to equal "Human wins!"
+//else if(this.player.choice === rock && this.computer.choice === scissors) {
+//this.player.wins++;
+//update innerText to equal "Human wins!";
+//} else if(this.player.choice === scissors && this.computer.choice === paper) {
+//
+
+}
+
+}
+
+
+
+}
