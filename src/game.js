36ea8615b150c6c47bfc07ec357cce117f3ec5ac
Stevie Ambroise <s.ambroise@protonmail.com>
2021-11-14 16:34:16 -0700

    Update game file to remove commented out code

--- src/game.js
+++ src/game.js
@@ -1,8 +1,8 @@
 class Game {
   constructor(type) {
     this.type = type;
-    this.player = new Player('human', '🙎🏾‍♀️');
-    this.computer = new Player('computer', '🖥');
+    this.player = new Player('Human', '🙎🏾‍♀️');
+    this.computer = new Player('Computer', '🖥');
     this.winner = "";
     this.classicChoices = ['book', 'ball', 'wand'];
     this.difficultChoices = ['book', 'crystalBall', 'wand', 'broom', 'hat'];
@@ -92,18 +92,3 @@ class Game {
   )
   }
 }
-
-
-//Game Logic
-//This could be an ELSE??? //if (this.player.choice === this.computer.choice) {
-// update the innerText of Choose Your Game to equal "It's a Draw"
-//this.player.wins
-//this.computer.wins
-//else if(this.player.choice === paper && this.computer.choice === rock) {
-//this.player.wins++;
-//update innerText to equal "Human wins!"
-//else if(this.player.choice === rock && this.computer.choice === scissors) {
-//this.player.wins++;
-//update innerText to equal "Human wins!";
-//} else if(this.player.choice === scissors && this.computer.choice === paper) {
-//
